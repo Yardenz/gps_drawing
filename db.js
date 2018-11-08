08099cc05c1596b23e631055fadaa0514ff2434e
Yardenz <zamir.yarden@gmail.com>
2018-11-07 23:49:13 -0500

    db add

--- db.js
+++ db.js
@@ -1,1 +1,52 @@
-db.js
+let base = "https://api.mlab.com/api/1/databases/"
+let apiKey = "JMfiQyspEkwFESsLp4CNlf3lMRlws9Li"
+let database = "drawing-db1"
+
+class DB {
+
+    get(f) {
+        console.log('GET')
+        let url = base + database + "/collections/paths?apiKey=" + apiKey
+        let request = new XMLHttpRequest()
+        request.open('GET', url)
+        request.onload = function() {
+          if (this.status >= 200 && this.status < 400) {
+            f(JSON.parse(this.response))
+          } else {
+            console.log(this.status)
+            console.log(this.response)
+          }
+        }
+        request.onerror = function() {
+            console.log(this.status)
+            console.log(this.response)
+        }
+        request.send()
+    }
+
+    insert(path) {
+        console.log("INSERT")
+        let url = base + database + "/collections/paths?apiKey=" + apiKey
+        let request = new XMLHttpRequest()
+        request.open('POST', url)
+        request.setRequestHeader('Content-Type', 'application/json')
+        request.onload = function() {
+          if (this.status >= 200 && this.status < 400) {
+            console.log(this.response)
+          } else {
+            console.log(this.status)
+            console.log(this.response)
+          }
+        }
+        request.onerror = function() {
+            console.log(this.status)
+            console.log(this.response)
+        }
+        let encoded = JSON.stringify({path: path})
+        console.log(encoded)
+        request.send(encoded)
+    }
+
+}
+
+let db = new DB()
